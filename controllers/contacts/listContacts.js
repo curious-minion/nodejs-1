892c2861f5f1636a092506fb1f2716a95fba7962
courios-minion <okarskaolya@gmail.com>
2021-09-28 12:48:33 +0200

    changed structure

---
+++ controllers/contacts/listContacts.js
@@ -0,0 +1,15 @@
+const fs = require("fs/promises");
+const path = require("path");
+const contactsPath = path.join("db", "contacts.json");
+
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = listContacts;
--- index.js
+++ index.js
@@ -1,57 +1,80 @@
 const { program } = require("commander");
 
-const contactsOperations = require("./contacts");
+const contactsOperations = require("./controllers/contacts/index");
 
 program
-    .option('-a, --action <type>', 'choose action')
-    .option('-i, --id <type>', 'user id')
-    .option('-n, --name <type>', 'user name')
-    .option('-e, --email <type>', 'user email')
-    .option('-p, --phone <type>', 'user phone');
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
 program.parse(process.argv);
 
 const options = program.opts();
 
-
 async function invokeAction({ action, id, name, email, phone }) {
-    switch (action) {
-        case 'list':
-            const contacts = await contactsOperations.listContacts();
-            console.table(contacts);
-            break;
+  switch (action) {
+    case "list":
+      try {
+        const contacts = await contactsOperations.listContacts();
+        console.table(contacts);
+      } catch (error) {
+        console.log(error.message);
+      }
 
-        case 'get':
-            const contactById = await contactsOperations.getContactById(id);
-            if (!contactById) {
-                console.log(`User with the id=${id} was not found`);
-            }
-            console.log(contactById);
-            break;
+      break;
 
-        case 'add':
-            if (!name || !email || !phone) {
-                console.log("Please type in user's name, email and phone number");
-                break;
-            }
-            const newContact = await contactsOperations.addContact({ name, email, phone });
-            console.log(newContact);
-            break;
+    case "get":
+      try {
+        const contactById = await contactsOperations.getContactById(id);
+        if (!contactById) {
+          console.log(`User with the id=${id} was not found`);
+        }
+        console.log(contactById);
+      } catch (error) {
+        console.log(error.message);
+      }
 
-        case 'remove':
-            const removeContact = await contactsOperations.removeContact(id);
+      break;
 
-            if (!removeContact) {
-                console.log("Please choose the existing user's id to proceed with the deletion");
-            }
-            console.log(`User with the id: ${id} was deleted`);
-            break;
+    case "add":
+      try {
+        if (!name || !email || !phone) {
+          console.log("Please type in user's name, email and phone number");
+          break;
+        }
+        const newContact = await contactsOperations.addContact({
+          name,
+          email,
+          phone,
+        });
+        console.log(newContact);
+      } catch (error) {
+        console.log(error.message);
+      }
 
-        default:
-            console.warn('\x1B[31m Unknown action type!');
-    }
+      break;
+
+    case "remove":
+      try {
+        const removeContact = await contactsOperations.removeContact(id);
+
+        if (!removeContact) {
+          console.log(
+            "Please choose the existing user's id to proceed with the deletion"
+          );
+        }
+        console.log(`User with the id: ${id} was deleted`);
+      } catch (error) {
+        console.log(error.message);
+      }
+
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
 }
 
 invokeAction(options);
-
-
